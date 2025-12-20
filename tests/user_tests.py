b0d82c3a1a6256f2d82c2360d9ee797ad1cefd86
inesbchainho <inesbchainho@gmail.com>
2025-12-20 22:55:57 +0000

    Update user_tests.py

--- tests/user_tests.py
+++ tests/user_tests.py
@@ -1,7 +1,6 @@
 import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
-from rag.vector_search import vector_search
 from services.user_creation import create_profile, users_collection
 
 new_user = create_profile(name="Inês", email="test@example.com", country="Portugal", city="Lisbon", age=30)
