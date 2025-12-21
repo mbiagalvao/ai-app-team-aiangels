6b40186ed19582cfef4ed220a758a052185f9948
inesbchainho <inesbchainho@gmail.com>
2025-12-21 15:37:17 +0000

    moremoremore

--- tests/user_tests.py
+++ tests/user_tests.py
@@ -1,4 +1,20 @@
-from services.user_creation import (create_profile, get_profile, update_profile, delete_profile, users_collection)
+from pymongo import MongoClient
+import os
+from bson import ObjectId
+from dotenv import load_dotenv
+from services.user_creation import (create_profile, get_profile, update_profile, delete_profile)
+
+load_dotenv() # loads .env into process env
+
+mongo_uri = os.getenv("MONGODB_URI")
+if not mongo_uri:
+    raise ValueError("⚠️ MONGODB_URI not found in environment variables")
+
+mongo_client = MongoClient(mongo_uri)
+db = mongo_client["catastrophe_db"]
+users_collection = db["users"]
+
+print("✅ Connected to MongoDB")
 
 new_user_id = create_profile(name = "Inês", email = "test@example.com", country = "Portugal", city = "Lisbon", age = 30)
 print(new_user_id)
